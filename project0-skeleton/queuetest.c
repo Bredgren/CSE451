1914cf7b5da1c6d9eb534af831d7668a615d0755
Brandon Edgren <brandon.edgren@gmail.com>
2014-01-13 13:48:55 -0800

    Add tests for empty and reverse

--- project0-skeleton/queuetest.c
+++ project0-skeleton/queuetest.c
@@ -8,14 +8,13 @@ typedef struct _order {
   int* errors;
 } order;
 
-bool confirm_order(queue_element* elem, queue_function_args* args) {
+bool confirm_order_fn(queue_element* elem, queue_function_args* args) {
   order* expected_order = (order*) args;
   int index = expected_order->index;
   int* errors = expected_order->errors;
   char* location = expected_order->location;
   queue_element* item = expected_order->expected[index];
 
-  //  printf("item %d, element %d\n", *(int*) item, *(int*) elem);
   if (item != elem) {
     printf("Incorrect order in %s\n", location);
     printf("    at index %d expeced %d, got %d\n", index,
@@ -27,7 +26,21 @@ bool confirm_order(queue_element* elem, queue_function_args* args) {
   return true;
 }
 
-void test_size(queue* q, int exp_size, char* location, int* errors) {
+void confirm_order(queue* q, queue_element** expected,
+                   char* location, int* errors) {
+  order expected_order;
+  expected_order.index = 0;
+  expected_order.expected = expected;
+  expected_order.location = location;
+  expected_order.errors = errors;
+
+  if (*errors > 0)
+    printf("\n");  // Adds new line between each set
+
+  queue_apply(q, confirm_order_fn, &expected_order);
+}
+
+void confirm_size(queue* q, int exp_size, char* location, int* errors) {
   size_t size = queue_size(q);
   if (size != exp_size) {
     printf("Queue size is incorrect in %s: expeceted %d, got %zu\n",
@@ -36,10 +49,18 @@ void test_size(queue* q, int exp_size, char* location, int* errors) {
   }
 }
 
+void confirm_empty(queue* q, bool expected, char* location, int* errors) {
+  if (queue_is_empty(q) != expected) {
+    printf("Queue emptiness not as expected in %s: expeceted %d, got %d\n",
+           location, expected, !expected);
+    (*errors)++;
+  }
+}
+
 void confirm_append(queue* q, queue_element* elem, int exp_size,
                     char* location, int* errors) {
   queue_append(q, elem);
-  test_size(q, exp_size, location, errors);
+  confirm_size(q, exp_size, location, errors);
 }
 
 // If exp_elem is NULL this will check test removing from an empty queue.
@@ -47,7 +68,7 @@ void confirm_remove(queue* q, queue_element* exp_elem, int exp_size,
                     char* location, int* errors) {
   queue_element* removed_elem;
   bool result = queue_remove(q, &removed_elem);
-  test_size(q, exp_size, location, errors);
+  confirm_size(q, exp_size, location, errors);
 
   if (exp_elem == NULL) {
     if (result) {
@@ -72,7 +93,7 @@ void test_append(int* errors) {
 
   queue* q = queue_create();
 
-  test_size(q, 0, location, errors);
+  confirm_size(q, 0, location, errors);
 
   int x = 0;
   int y = 1;
@@ -83,14 +104,17 @@ void test_append(int* errors) {
   confirm_append(q, &x, 4, location, errors);
 
   // Check the queue's order
-  order expected_order;
-  expected_order.index = 0;
-  queue_element* expected[] = {&x, &y, &z, &x};
-  expected_order.expected = expected;
-  expected_order.location = location;
-  expected_order.errors = errors;
+  /* order expected_order; */
+  /* expected_order.index = 0; */
+  /* queue_element* expected[] = {&x, &y, &z, &x}; */
+  /* expected_order.expected = expected; */
+  /* expected_order.location = location; */
+  /* expected_order.errors = errors; */
 
-  queue_apply(q, confirm_order, &expected_order);
+  /* queue_apply(q, confirm_order, &expected_order); */
+
+  queue_element* expected[] = {&x, &y, &z, &x};
+  confirm_order(q, expected, location, errors);
 
   queue_destroy(q);
   q = NULL;
@@ -101,7 +125,7 @@ void test_remove(int* errors) {
 
   queue* q = queue_create();
 
-  test_size(q, 0, location, errors);
+  confirm_size(q, 0, location, errors);
 
   int x = 0;
   int y = 1;
@@ -132,11 +156,67 @@ void test_remove(int* errors) {
   q = NULL;
 }
 
+void test_is_empty(int* errors) {
+  char* location = "test_is_empty";
+
+  queue* q = queue_create();
+
+  confirm_size(q, 0, location, errors);
+  confirm_empty(q, true, location, errors);
+
+  int x = 0;
+  int y = 1;
+  confirm_append(q, &x, 1, location, errors);
+  confirm_empty(q, false, location, errors);
+  confirm_append(q, &y, 2, location, errors);
+  confirm_empty(q, false, location, errors);
+
+  confirm_remove(q, &x, 1, location, errors);
+  confirm_empty(q, false, location, errors);
+  confirm_remove(q, &y, 0, location, errors);
+  confirm_empty(q, true, location, errors);
+
+  queue_destroy(q);
+  q = NULL;
+}
+
+void test_reverse(int* errors) {
+  char* location = "test_remove";
+
+  queue* q = queue_create();
+
+  confirm_size(q, 0, location, errors);
+
+  int x = 0;
+  int y = 1;
+  int z = 2;
+  confirm_append(q, &x, 1, location, errors);
+  queue_element* expected1[] = {&x};
+  confirm_order(q, expected1, location, errors);
+
+  confirm_append(q, &y, 2, location, errors);
+  queue_element* expected2[] = {&y, &x};
+  confirm_order(q, expected2, location, errors);
+
+  confirm_append(q, &z, 3, location, errors);
+  queue_element* expected3[] = {&z, &x, &y};
+  confirm_order(q, expected3, location, errors);
+
+  confirm_append(q, &x, 4, location, errors);
+  queue_element* expected4[] = {&x, &y, &x, &z};
+  confirm_order(q, expected4, location, errors);
+
+  queue_destroy(q);
+  q = NULL;
+}
+
 int main(int argc, char* argv[]) {
   int errors = 0;
 
   test_append(&errors);
   test_remove(&errors);
+  test_is_empty(&errors);
+  test_reverse(&errors);
 
   if (errors == 0)
     printf("All tests passed\n");
